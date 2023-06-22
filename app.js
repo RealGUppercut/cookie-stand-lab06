3d07cad91e5184b98d5b563913870aefeb871cb1
JoeVenton <joe.venton@gmail.com>
2023-06-22 16:25:54 +0100

    added print to html functionality

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict";
+
+const container = document.getElementById("container");
 const openHours = ["06:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"];
 console.log(openHours.length);
 
@@ -11,37 +13,44 @@ const seattle = {
     hourlyCookie: [],
     dailyCookies: 0,
 
-    calcRandomCx: function() {
+    calcRandomCx() {
         for (let i = 0; i < openHours.length; i++) {
         this.randomCx.push(generateRandomCx(this.minCx,this.maxCx));
         }
-        console.log(this.randomCx);
     },
-
+    
     calcHourlyCookie: function() {
         for (let i = 0; i < openHours.length; i++) {
         const hourSales = Math.ceil(this.randomCx[i]*this.MeanCookie);
-        this.hourlyCookie.push(hourSales)
         this.dailyCookies += hourSales;
-
+        this.hourlyCookie.push(hourSales)
+        }
+    },
+        render: function() {
+        this.calcRandomCx();
+        this.calcHourlyCookie();
+        
+        const article = document.createElement("article");
+        container.appendChild(article);
+    
+        const h3 = document.createElement("h3");
+        h3.textContent = this.location;
+        article.appendChild(h3);
+    
+        const ul = document.createElement("ul");
+        article.appendChild(ul);
+    
+        for (let i = 0; i < openHours.length; i++) {
+          const li = document.createElement("li");
+          li.textContent = `${openHours[i]}: ${this.hourlyCookie[i]} cookies`;
+          ul.appendChild(li);
+        }
+        const li = document.createElement("li");
+        li.textContent = `Total Cookies: ${this.dailyCookies}`;
+        ul.appendChild(li);
+        }
     }
-    console.log(this.hourlyCookie);
-    console.log(this.dailyCookies);
+    function generateRandomCx(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
-}
-
-function generateRandomCx(min,max) {
-    return ((Math).floor(Math.random() * (max - min + 1) + min));
-}
-
-
-
-// function hourlyCookie(Cx,Cookie) {
-//     const hourlyCookieArray = [];
-//     for (let i = 0; i < openHours.length; i++) {
-//         hourlyCookieArray.push((Cx * Cookie));
-//     }
-// }
-
-seattle.calcRandomCx();
-seattle.calcHourlyCookie();
+    seattle.render();
